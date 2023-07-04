8e3ba9c2ca9e9a5c5ac13fd7fd00e91f214fd3c6
Valeria Oreshko <oreshko-v@yandex.ru>
2023-07-04 14:56:12 +0300

    Second commit

---
+++ src/test/java/ru/netology/MovieManagerTest.java
@@ -0,0 +1,37 @@
+package ru.netology;
+
+import com.sun.nio.sctp.Association;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MovieManagerTest {
+
+    @Test
+    public void findAll() {
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("Film I");
+        manager.addMovie("Film II");
+        manager.addMovie("Film III");
+
+        String[] expected = {"Film I", "Film II", "Film III"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void findLast() {
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("Film I");
+        manager.addMovie("Film II");
+        manager.addMovie("Film III");
+
+        String[] expected = {"Film III", "Film II", "Film I"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+}
